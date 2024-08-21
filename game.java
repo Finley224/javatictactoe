dff95e3d36cb360fb8773f338b38953bb0e73eab
Finley <161162126+xavier224@users.noreply.github.com>
2024-08-21 14:29:10 -0400

    Began working on the play method

--- game.java
+++ game.java
@@ -2,12 +2,30 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Scanner;
 
-public class tictactoe {
+public class game {
     public static void printGrid(Dictionary<Integer, String> gameGrid) {
-        System.out.println(" " + gameGrid.get(1) + " | " + gameGrid.get(2) + " | " + gameGrid.get(3) + " \n---+---+---\n " + gameGrid.get(4) + " | " + gameGrid.get(5) + " | " + gameGrid.get(6) + " \n---+---+---\n " + gameGrid.get(7) + " | " + gameGrid.get(8) + " | " + gameGrid.get(9) + " ");
+        System.out.println(
+            " " + gameGrid.get(1) + " | " + gameGrid.get(2) + " | " + gameGrid.get(3)
+                + " \n---+---+---\n " + gameGrid.get(4) + " | " + gameGrid.get(5) + " | " + gameGrid.get(6)
+                + " \n---+---+---\n " + gameGrid.get(7) + " | " + gameGrid.get(8) + " | " + gameGrid.get(9) + " ");
     }
+
+    public static void printPlayGrid(Dictionary<Integer, String> numGrid, Dictionary<Integer, String> gameGrid, String player) {
+        System.out.println(
+            " " + numGrid.get(1) + " | " + numGrid.get(2) + " | " +numGrid.get(3) + "  ||  " + gameGrid.get(1) + " | " + gameGrid.get(2) + " | " + gameGrid.get(3)
+                + " \n---+---+--- || ---+---+---\n " + numGrid.get(4) + " | " + numGrid.get(5) + " | " + numGrid.get(6) + "  ||  " + gameGrid.get(4) + " | " + gameGrid.get(5) + " | " + gameGrid.get(6)
+                + " \n---+---+--- || ---+---+---\n " + numGrid.get(7) + " | " + numGrid.get(8) + " | " + numGrid.get(9) + "  ||  " + gameGrid.get(7) + " | " + gameGrid.get(8) + " | " + gameGrid.get(9));
+        System.out.println("Current player is: " + player);
+        System.out.print("Pick where to make a move, enter the number of the spot you want to move to: ");
+}
+
     public static void main(String[] args) {
         Dictionary<Integer, String> gameGrid = new Hashtable<>();
+
+        String currentPlayer = "O";
+        String player1Name;
+        String player2Name;
+
         // Setup the gamegrid for the new game
         gameGrid.put(1, " ");
         gameGrid.put(2, " ");
@@ -32,8 +50,11 @@ public class tictactoe {
         numGrid.put(9, "9");
 
         Scanner getMove = new Scanner(System.in);
-        printGrid(numGrid);
-        System.out.println("Pick where to make a move, enter the number of the spot you want to move to.");
+        printPlayGrid(numGrid, gameGrid, currentPlayer);
+        int moveLoc = getMove.nextInt();
+        getMove.close();
+        gameGrid.put(moveLoc, currentPlayer);
+        printPlayGrid(numGrid, gameGrid, currentPlayer);
 
     }
 }
