d0394e2ffaa108ce766b57e475e6d3626df9b634
Finley <161162126+xavier224@users.noreply.github.com>
2024-08-22 10:31:58 -0400

    Most Game Loop Logic done
    Also added win check placeholder

--- game.java
+++ game.java
@@ -10,13 +10,18 @@ public class game {
                 + " \n---+---+---\n " + gameGrid.get(7) + " | " + gameGrid.get(8) + " | " + gameGrid.get(9) + " ");
     }
 
+    public static String checkWin(Dictionary<Integer, String> gameGrid) {
+        // Placeholder Code
+        return "";
+    }
+
     public static void printPlayGrid(Dictionary<Integer, String> numGrid, Dictionary<Integer, String> gameGrid, String player) {
         System.out.println(
             " " + numGrid.get(1) + " | " + numGrid.get(2) + " | " +numGrid.get(3) + "  ||  " + gameGrid.get(1) + " | " + gameGrid.get(2) + " | " + gameGrid.get(3)
                 + " \n---+---+--- || ---+---+---\n " + numGrid.get(4) + " | " + numGrid.get(5) + " | " + numGrid.get(6) + "  ||  " + gameGrid.get(4) + " | " + gameGrid.get(5) + " | " + gameGrid.get(6)
                 + " \n---+---+--- || ---+---+---\n " + numGrid.get(7) + " | " + numGrid.get(8) + " | " + numGrid.get(9) + "  ||  " + gameGrid.get(7) + " | " + gameGrid.get(8) + " | " + gameGrid.get(9));
         System.out.println("Current player is: " + player);
-        System.out.print("Pick where to make a move, enter the number of the spot you want to move to: ");
+        System.out.println("Pick where to make a move, enter the number of the spot you want to move to (0 to quit game): ");
 }
 
     public static void main(String[] args) {
@@ -25,6 +30,7 @@ public class game {
         String currentPlayer = "O";
         String player1Name;
         String player2Name;
+        String winner = "none";
 
         // Setup the gamegrid for the new game
         gameGrid.put(1, " ");
@@ -50,11 +56,29 @@ public class game {
         numGrid.put(9, "9");
 
         Scanner getMove = new Scanner(System.in);
-        printPlayGrid(numGrid, gameGrid, currentPlayer);
-        int moveLoc = getMove.nextInt();
-        getMove.close();
-        gameGrid.put(moveLoc, currentPlayer);
-        printPlayGrid(numGrid, gameGrid, currentPlayer);
 
+        while (winner == "none") {
+            printPlayGrid(numGrid, gameGrid, currentPlayer);
+            int moveLoc = getMove.nextInt();
+            getMove.nextLine();
+            if (checkWin(gameGrid) == "X") {
+                winner = "X";
+            } else if (checkWin(gameGrid) == "O") {
+                winner = "O";
+            } else if (gameGrid.get(moveLoc) == " ") {
+                gameGrid.put(moveLoc, currentPlayer);
+
+                if (currentPlayer == "O") {
+                    currentPlayer = "X";
+                } else {
+                    currentPlayer = "O";
+                }
+            } else if (moveLoc == 0) {
+                winner = "Program Quit";
+            } else {
+                System.out.println("Invalid Move Location");
+            }
+        }
+        getMove.close();
     }
 }
